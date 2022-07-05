23256bb23927fee74fb94e5a10a6579792a60ecf
Manuel Rivas <mrivasmahecha2003@gmail.com>
2022-07-05 08:41:00 -0400

    Added functionality to the dice button, and also a little animation

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const adviceSlipAPIUrl = 'https://api.adviceslip.com/advice';
+
+const adviceIdSpan = document.querySelector('#adviceIdSpan');
+const adviceTextParagraph = document.querySelector('#adviceTextParagraph');
+
+const adviceDiceButton = document.querySelector('#rolAdviceDice');
+
+function updateAdvice() {
+  let req = new XMLHttpRequest();
+
+  req.open('GET', adviceSlipAPIUrl);
+
+  req.onreadystatechange = () => {
+    if (req.readyState !== 4 || req.status !== 200) return;
+
+    const responseBody = JSON.parse(req.responseText).slip;
+
+    adviceIdSpan.textContent = responseBody.id;
+    adviceTextParagraph.textContent = `"${responseBody.advice}"`;
+
+    adviceDiceButton.classList.toggle('fetching');
+    adviceDiceButton.disabled = false;
+  };
+
+  req.send();
+
+  adviceDiceButton.classList.toggle('fetching');
+  adviceDiceButton.disabled = true;
+}
+
+adviceDiceButton.addEventListener('click', updateAdvice);
+
+updateAdvice();
